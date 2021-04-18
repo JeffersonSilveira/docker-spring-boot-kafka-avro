0b14302032b30f4939ea064e9c49097864eda24c
JeffersonSilveira <jeffersonsilveira30@gmail.com>
2021-04-18 01:31:04 -0300

    Ajuste de fluxo

--- src/main/java/com/springboot/kafka/avro/consumer/UserConsumer.java
+++ src/main/java/com/springboot/kafka/avro/consumer/UserConsumer.java
@@ -5,7 +5,11 @@ import lombok.extern.apachecommons.CommonsLog;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @CommonsLog(topic = "Consumer Logger")
@@ -15,7 +19,13 @@ public class UserConsumer {
     private String topicName;
 
     @KafkaListener(topics = "users", groupId = "group_id")
-    public void consume(ConsumerRecord<String, User> record) {
-        log.info(String.format("Consumed message -> %s", record.value()));
+    public void consume(String user,
+                               @Header(KafkaHeaders.RECEIVED_PARTITION_ID) List partitions,
+                               @Header(KafkaHeaders.RECEIVED_TOPIC) List topics,
+                               @Header(KafkaHeaders.OFFSET) List offsets) {
+        System.out.printf("Topic::: %s  partitions---%d[ offsets::%d] \"user::::%s\"\n", topics.get(0), partitions.get(0), offsets.get(0), user);
     }
+//    public void consume(ConsumerRecord<String, User> record) {
+//        log.info(String.format("Consumed message -> %s", record.value()));
+//    }
 }
--- src/main/java/com/springboot/kafka/avro/producer/UserProducer.java
+++ src/main/java/com/springboot/kafka/avro/producer/UserProducer.java
@@ -2,10 +2,15 @@ package com.springboot.kafka.avro.producer;
 
 import io.confluent.developer.User;
 import lombok.extern.apachecommons.CommonsLog;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Service;
+import java.util.UUID;
 
 @Service
 @CommonsLog(topic = "Producer Logger")
@@ -22,7 +27,8 @@ public class UserProducer {
     }
 
     public void sendMessage(User user) {
-        this.kafkaTemplate.send(TOPIC, user.getName(), user);
+        final String mensageKey = UUID.randomUUID().toString();
+        this.kafkaTemplate.send(TOPIC, mensageKey, user);
         log.info(String.format("Produced user -> %s", user));
     }
 }
